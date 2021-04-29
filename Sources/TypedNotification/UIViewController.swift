00dd801121213c42520cb8cd8ffa8f475d30db0e
David Southgate <d@davidsouthgate.co.uk>
2021-04-29 22:49:45 +0100

    Added addObserver for notification name on UIViewController

--- Sources/TypedNotification/UIViewController.swift
+++ Sources/TypedNotification/UIViewController.swift
@@ -11,6 +11,11 @@ public extension UIViewController  {
     func addObserver<N>(_ forType: N.Type, queue: OperationQueue? = nil, using block: @escaping (N) -> Void) -> NSObjectProtocol where N : TypedNotification {
         return NotificationCenter.default.addObserver(forType, using: block)
     }
+
+    @discardableResult
+    public func addObserver(forName name: NSNotification.Name?, using block: @escaping (Notification) -> Void) -> NSObjectProtocol {
+        return NotificationCenter.default.addObserver(forName: name, object: nil, queue: nil, using: block)
+    }
     
     func removeObserver(_ observer: NSObjectProtocol) {
         NotificationCenter.default.removeObserver(observer)
